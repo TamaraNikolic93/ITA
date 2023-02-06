c4fbbc511ca5aa8c988f82444bcf872e6fc853e4
Tamara Nikolić <tamara.nikolic@clarivate.com>
2023-02-06 12:58:19 +0100

    Initial Commit

---
+++ JavaAssignment.java
@@ -0,0 +1,13 @@
+public class ITAassignment
+{
+
+	public static void main(String[] args)
+	{
+        
+		for (int i = 0; i < 36; i++) {
+            	System.out.println("Hello World");
+        	}
+
+	}
+
+}
